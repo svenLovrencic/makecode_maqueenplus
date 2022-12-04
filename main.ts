a681e16bc82cbce89a39de87615c19f63c7193a1
svenLovrencic <37621509+svenLovrencic@users.noreply.github.com>
2022-12-04 14:53:48 +0100

    Basic movement for 2 seconds

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let move_x_ms = input.runningTime() + 2000
+while (input.runningTime() < move_x_ms) {
+    DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 100)
+}
+DFRobotMaqueenPlus.mototStop(Motors.ALL)
 basic.forever(function () {
 	
 })
